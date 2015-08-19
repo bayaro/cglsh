35cd656e3df10a6554cbbf52e4d197e377028a96
Baya <baya@rodovid.org>
2015-08-19 13:03:15 +0300

    + stop on ^C

--- life.sh
+++ life.sh
@@ -6,19 +6,26 @@ function draw() {
     echo -n '[?25l' # hide cursor
 }
 
-stty raw
+function bye_bye() {
+    stty sane
+    echo '[?25h' # show cursor
+}
+
+trap bye_bye SIGINT SIGTERM INT EXIT
+
+stty raw -echo
 echo -n 'c' # VT100 reset
 echo '[?71' # turn off auto-wrap
 echo '' # clear screen
 echo -n '[?25l' # hide cursor
 
-AGE=3
+AGE=10
 while [ $AGE -gt 0 ] ; do
     draw 20 20 "AGE: $AGE"
 
     sleep 1
+    if [ "`dd bs=1 count=1 iflag=nonblock status=none 2>/dev/null`" == "" ]; then # stop on ^C
+        break
+    fi
     AGE=$(( $AGE - 1 ))
 done
-
-stty sane
-echo '[?25h' # show cursor
