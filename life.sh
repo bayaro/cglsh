704de4a365a76707fb3d1ccdc3b6bfe8c0a5cd56
Baya <baya@rodovid.org>
2015-08-19 11:26:03 +0300

    + draw msg at chosen position

--- life.sh
+++ life.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+function draw() {
+    echo -n "[$1;${2}H" # set cursor position
+    echo -n "$3" # show string
+    echo -n '[?25l' # hide cursor
+}
+
 stty raw
 echo -n 'c' # VT100 reset
 echo '[?71' # turn off auto-wrap
 echo '' # clear screen
 echo -n '[?25l' # hide cursor
+
+draw 20 20 "`date`"
 sleep 3
+
 stty sane
 echo '[?25h' # show cursor
